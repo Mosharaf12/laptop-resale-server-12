0c7b1106872144ac8dc1707d04efed7683b23636
unknown <mosharafctg111@gmail.com>
2022-11-26 00:39:58 +0600

    added the detailsCard get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -40,6 +40,14 @@ async function run(){
       const result = await usersCollection.insertOne(user)
       res.send(result)
 
+    })
+
+    app.get('/laptop/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const result = await usedLaptopsCollection.findOne(query)
+      res.send(result)
+      console.log(result);
     })
 
   }
